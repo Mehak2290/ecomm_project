9620a0a099c6d49df553c5845181bdeec1c5b61a
Mehak2290 <140011232+Mehak2290@users.noreply.github.com>
2024-04-17 14:17:03 -0500

    Updating about_page.rb

--- about_page.rb
+++ about_page.rb
@@ -2,6 +2,6 @@ class AboutPage < ApplicationRecord
     # Define which attributes are searchable
     def self.ransackable_attributes(auth_object = nil)
       %w[title body created_at updated_at]
-    end
   end
+ end
   
